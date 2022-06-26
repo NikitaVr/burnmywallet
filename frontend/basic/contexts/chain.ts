36ab7e308e317800426fb09b523274cb891dfdf7
Nikita Varabei <nVarabei@gmail.com>
2022-06-25 21:46:38 -0400

    add contract address to Chain

--- frontend/basic/contexts/chain.ts
+++ frontend/basic/contexts/chain.ts
@@ -3,16 +3,19 @@ import { createContext } from "react";
 export type Chain = {
   name: "rinkeby" | "mumbai" | "optimism";
   chainId: number;
+  contractAddress: string;
 };
 
 export const chains: Record<string, Chain> = {
   rinkeby: {
     name: "rinkeby",
     chainId: 4,
+    contractAddress: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS!,
   },
   mumbai: {
     name: "mumbai",
     chainId: 137,
+    contractAddress: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS_POLYGON_MUMBAI!,
   },
 };
 
