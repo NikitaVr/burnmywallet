438a0740194e1bf19c2ca4952f98f1c9771cc89a
Christian Koopmann <christian@MTMAC0057.local>
2022-07-04 19:00:01 +1000

    Adjust hardhat code according to example

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -7,7 +7,9 @@ require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
+require("hardhat-preprocessor");
 require("solidity-coverage");
+const fs = require("fs");
 
 const {
   RINKEBY_API_URL,
@@ -19,18 +21,26 @@ const {
   ROYALTY_RECEIVER_ADDR,
 } = process.env;
 
-// THIS IS A PUBLICLY KNOWN (HARDHAT) PRIVATE KEY. 
+function getRemappings() {
+  const remappings = fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean)
+    .map((line) => line.trim().split("="));
+  return remappings;
+}
+
+// THIS IS A PUBLICLY KNOWN (HARDHAT) PRIVATE KEY.
 // DO NOT USE THIS IN PRODUCTION OR SEND ANY FUNDS TO THE ASSOCIATED ADDRESS
 const BACKUP_PRIVATE_KEY = "f2f48ee19680706196e2e339e5da3491186e0c4c5030670656b0e0164837257d";
 
 let PRIVATE_KEY;
-if(process.env.PRIVATE_KEY) {
-    PRIVATE_KEY = process.env.PRIVATE_KEY;
+if (process.env.PRIVATE_KEY) {
+  PRIVATE_KEY = process.env.PRIVATE_KEY;
 } else {
-    console.log("Using backup private key");
-    PRIVATE_KEY = BACKUP_PRIVATE_KEY;
+  console.log("Using backup private key");
+  PRIVATE_KEY = BACKUP_PRIVATE_KEY;
 }
-
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -83,5 +93,25 @@ module.exports = {
       mainnet: ETHERSCAN_API_KEY,
       polygon: POLYGONSCAN_API_KEY,
     },
+  },
+  paths: {
+    sources: "./src", // Use ./src rather than ./contracts as Hardhat expects
+    cache: "./cache_hardhat", // Use a different cache for Hardhat than Foundry
+  },
+  // This fully resolves paths for imports in the ./lib directory for Hardhat
+  preprocess: {
+    eachLine: () => ({
+      transform: (line) => {
+        if (line.match(/^\s*import /i)) {
+          getRemappings().forEach(([find, replace]) => {
+            if (line.match(find)) {
+              console.log("replacing", { find, replace });
+              line = line.replace(find, replace);
+            }
+          });
+        }
+        return line;
+      },
+    }),
   },
 };
